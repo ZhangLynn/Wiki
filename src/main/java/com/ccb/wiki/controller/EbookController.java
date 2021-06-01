2fcdc6e26085e8557daa5cba9fb2059a561ad6f3
zl8312070@sina.com <zl...8312070>
2021-06-01 09:02:13 +0800

    feat: 文档的新增、删除、保存功能

--- src/main/java/com/ccb/wiki/controller/EbookController.java
+++ src/main/java/com/ccb/wiki/controller/EbookController.java
@@ -1,15 +1,14 @@
 package com.ccb.wiki.controller;
 
 import com.ccb.wiki.req.EbookQueryReq;
+import com.ccb.wiki.req.EbookSaveReq;
 import com.ccb.wiki.resp.CommonResp;
 import com.ccb.wiki.resp.EbookQueryResp;
 import com.ccb.wiki.resp.PageResp;
 import com.ccb.wiki.service.EbookService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
@@ -27,6 +26,20 @@ public class EbookController {
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp();
         PageResp<EbookQueryResp> list = ebookService.list(req);
         resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp list(@Valid @RequestBody EbookSaveReq req) {
+        CommonResp resp = new CommonResp();
+        ebookService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        ebookService.delete(id);
         return resp;
     }
 
--- src/main/java/com/ccb/wiki/service/EbookService.java
+++ src/main/java/com/ccb/wiki/service/EbookService.java
@@ -4,11 +4,15 @@ import com.ccb.wiki.domain.Ebook;
 import com.ccb.wiki.domain.EbookExample;
 import com.ccb.wiki.mapper.EbookMapper;
 import com.ccb.wiki.req.EbookQueryReq;
+import com.ccb.wiki.req.EbookSaveReq;
 import com.ccb.wiki.resp.EbookQueryResp;
 import com.ccb.wiki.resp.PageResp;
 import com.ccb.wiki.util.CopyUtil;
+import com.ccb.wiki.util.SnowFlake;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -18,7 +22,10 @@ import java.util.List;
 @Service
 public class EbookService {
 
-//    private static final Logger LOG = LoggerFactory.getLogger();
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
+    @Resource
+    private SnowFlake snowFlake;
 
     @Resource
     private EbookMapper ebookMapper;
@@ -40,4 +47,21 @@ public class EbookService {
         pageResp.setTotal(pageInfo.getTotal());
         return pageResp;
     }
+
+    public void save(EbookSaveReq req) {
+        Ebook ebook = CopyUtil.copy(req, Ebook.class);
+        if (ObjectUtils.isEmpty(req.getId())) {
+            // 新增
+            ebook.setId(snowFlake.nextId());
+            ebookMapper.insert(ebook);
+        } else {
+            // 更新
+            ebookMapper.updateByPrimaryKey(ebook);
+        }
+    }
+
+    public void delete(Long id) {
+        ebookMapper.deleteByPrimaryKey(id);
+    }
+
 }
