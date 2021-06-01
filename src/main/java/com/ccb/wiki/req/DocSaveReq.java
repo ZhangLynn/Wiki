24b9a2c2c34db21396b757e83320fc705db3fde3
zl8312070@sina.com <zl...8312070>
2021-06-01 16:53:31 +0800

    feat: 文档save请求做参数非空校验

--- src/main/java/com/ccb/wiki/req/DocSaveReq.java
+++ src/main/java/com/ccb/wiki/req/DocSaveReq.java
@@ -1,14 +1,20 @@
 package com.ccb.wiki.req;
 
+import javax.validation.constraints.NotNull;
+
 public class DocSaveReq {
     private Long id;
 
+    @NotNull(message = "【电子书】不能为空")
     private Long ebookId;
 
+    @NotNull(message = "【父文档】不能为空")
     private Long parent;
 
+    @NotNull(message = "【名称】不能为空")
     private String name;
 
+    @NotNull(message = "【顺序】不能为空")
     private Integer sort;
 
     private Integer viewCount;
