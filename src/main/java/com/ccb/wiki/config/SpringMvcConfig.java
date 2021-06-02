fa140753e490520e6839c6a48f3ac486b0164003
zl8312070@sina.com <zl...8312070>
2021-06-02 18:42:07 +0800

    feat: 增加登录拦截器，配置spring拦截器使用，过滤部分接口

--- src/main/java/com/ccb/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/ccb/wiki/config/SpringMvcConfig.java
@@ -1,45 +1,46 @@
-//package com.ccb.wiki.config;
-//
-////import com.ccb.wiki.interceptor.ActionInterceptor;
+package com.ccb.wiki.config;
+
+//import com.ccb.wiki.interceptor.ActionInterceptor;
 //import com.ccb.wiki.interceptor.LogInterceptor;
-////import com.ccb.wiki.interceptor.LoginInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import javax.annotation.Resource;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
-////    @Resource
-////    LoginInterceptor loginInterceptor;
-////
-////    @Resource
-////    ActionInterceptor actionInterceptor;
+
+import com.ccb.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
 //
 //    @Resource
+//    ActionInterceptor actionInterceptor;
+
+//    @Resource
 //    LogInterceptor logInterceptor;
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**");
-////                .excludePathPatterns(
-////                        "/test/**",
-////                        "/redis/**",
-////                        "/user/login",
-////                        "/category/all",
-////                        "/ebook/list",
-////                        "/doc/all/**",
-////                        "/doc/vote/**",
-////                        "/doc/find-content/**",
-////                        "/ebook-snapshot/**"
-////                );
-//
-////        registry.addInterceptor(actionInterceptor)
-////                .addPathPatterns(
-////                        "/*/save",
-////                        "/*/delete/**",
-////                        "/*/reset-password");
-//    }
-//}
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+
+//        registry.addInterceptor(actionInterceptor)
+//                .addPathPatterns(
+//                        "/*/save",
+//                        "/*/delete/**",
+//                        "/*/reset-password");
+    }
+}
